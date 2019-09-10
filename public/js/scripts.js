7accf763be96c93b4ec9f516ee69867575316565
joshmakar <joshmakar@gmail.com>
2019-09-10 10:45:36 -0400

    Move JS from scripts.js to scripts.ts and fix errors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,60 +1,46 @@
 /*///////////////////////////////////////////////
 Top Nav Functionality
 ///////////////////////////////////////////////*/
-
 var toggler = document.getElementById('toggler');
 var navMenu = document.getElementById('nav-menu');
 toggler.addEventListener('click', function (element) {
-  navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
 });
-
-
 /*///////////////////////////////////////////////
 Generate Bar Graph
 ///////////////////////////////////////////////*/
-
 // JSON Data for Bar Graph
-var chartItem =[
-  {
-    label: "Public Medical School, In-State $34,592",
-    cost: 34592,
-    color: "#E57373"
-  },
-  {
-    label: "Public Medical School, Out-of-State $56,668",
-    cost: 58668,
-    color: "#64B5F6"
-  },
-  {
-    label: "Private Medical School $50,000",
-    cost: 50000,
-    color: "#4DB6AC"
-  },
-  {
-    label: "CAM Education $25,000",
-    cost: 25000,
-    color: "#FF8A65"
-  }
-]
-
+var chartItem = [
+    {
+        label: "Public Medical School, In-State $34,592",
+        cost: 34592,
+        color: "#E57373"
+    },
+    {
+        label: "Public Medical School, Out-of-State $56,668",
+        cost: 58668,
+        color: "#64B5F6"
+    },
+    {
+        label: "Private Medical School $50,000",
+        cost: 50000,
+        color: "#4DB6AC"
+    },
+    {
+        label: "CAM Education $25,000",
+        cost: 25000,
+        color: "#FF8A65"
+    }
+];
 // Output Bar Graph
 function renderTuitionGraph(data) {
-  const averageTuitionGraph = document.getElementById('average_tuition_graph');
-  const maxCost = Math.max.apply(Math, chartItem.map(object => object.cost));
-
-  data.forEach(statistic => {
-    const costPercentage = Math.round((statistic.cost / maxCost) * 100);
-    const graphData = `<dt data-aos="grow-right" class="percentage-${costPercentage}" style="background-color: ${statistic.color}">${statistic.label}</dt>`;
-    
-    averageTuitionGraph.innerHTML += graphData;
-  });
+    var averageTuitionGraph = document.getElementById('average_tuition_graph');
+    var maxCost = Math.max.apply(Math, chartItem.map(function (object) { return object.cost; }));
+    data.forEach(function (statistic) {
+        var costPercentage = Math.round((statistic.cost / maxCost) * 100);
+        var graphData = "<dt data-aos=\"grow-right\" class=\"percentage-" + costPercentage + "\" style=\"background-color: " + statistic.color + "\">" + statistic.label + "</dt>";
+        averageTuitionGraph.innerHTML += graphData;
+    });
 }
-
 renderTuitionGraph(chartItem);
-
-
-/*///////////////////////////////////////////////
-Animate on Scroll
-///////////////////////////////////////////////*/
-
 AOS.init();
--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,8 +1,62 @@
-const toggler = document.getElementById('toggler');
-const navMenu = document.getElementById('nav-menu');
+/*///////////////////////////////////////////////
+Top Nav Functionality
+///////////////////////////////////////////////*/
 
-toggler.addEventListener('click', (element) => {
+var toggler = document.getElementById('toggler');
+var navMenu = document.getElementById('nav-menu');
+toggler.addEventListener('click', function (element) {
   navMenu.classList.toggle('active');
 });
+
+
+/*///////////////////////////////////////////////
+Generate Bar Graph
+///////////////////////////////////////////////*/
+
+// JSON Data for Bar Graph
+var chartItem =[
+  {
+    label: "Public Medical School, In-State $34,592",
+    cost: 34592,
+    color: "#E57373"
+  },
+  {
+    label: "Public Medical School, Out-of-State $56,668",
+    cost: 58668,
+    color: "#64B5F6"
+  },
+  {
+    label: "Private Medical School $50,000",
+    cost: 50000,
+    color: "#4DB6AC"
+  },
+  {
+    label: "CAM Education $25,000",
+    cost: 25000,
+    color: "#FF8A65"
+  }
+]
+
+// Output Bar Graph
+function renderTuitionGraph(data: { label: string; cost: number; color: string; }[]) {
+  const averageTuitionGraph = document.getElementById('average_tuition_graph');
+  const maxCost = Math.max.apply(Math, chartItem.map(object => object.cost));
+
+  data.forEach((statistic: { cost: number; color: string; label: string; }) => {
+    const costPercentage = Math.round((statistic.cost / maxCost) * 100);
+    const graphData = `<dt data-aos="grow-right" class="percentage-${costPercentage}" style="background-color: ${statistic.color}">${statistic.label}</dt>`;
+    
+    averageTuitionGraph.innerHTML += graphData;
+  });
+}
+
+renderTuitionGraph(chartItem);
+
+
+/*///////////////////////////////////////////////
+Animate on Scroll
+///////////////////////////////////////////////*/
+
+declare var AOS: any;
 
 AOS.init();
